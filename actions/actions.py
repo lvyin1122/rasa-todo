81f5fb0c6702ca6a455b400942938ff4166d3ab3
lvyin1122 <khiwhyzqd@outlook.com>
2022-10-29 23:03:42 +0800

    draft

--- actions/actions.py
+++ actions/actions.py
@@ -4,9 +4,11 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 from rasa_sdk.events import FollowupAction
 from rasa_sdk.events import BotUttered
-class AddTodo(Action):
+from calendar import calendar, calendar_api
+
+class CreateTodo(Action):
     def name(self) -> Text:
-        return "action_add_todo"
+        return "action_create_todo"
 
     async def run(
         self,
@@ -14,6 +16,79 @@ class AddTodo(Action):
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
+        todo = tracker.get_slot("todo")
+        todo_list = tracker.get_slot("todo_list")
+        todo_list.append(todo)
+        print(todo_list)
+        dispatcher.utter_message(response="utter_todo_added", todo=todo)
+        return [SlotSet("todo", None), SlotSet("todo_list", todo_list)]
+    
+class ListTodo(Action):
+    def name(self) -> Text:
+        return "action_list_todo"
 
-        dispatcher.utter_message(template="utter_todo_added")
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        todo_list = tracker.get_slot("todo_list")
+        print(todo_list)
+        todo_list = "".join([f"\n {idx+1}. {todo}" for idx,todo in enumerate(todo_list)])
+        dispatcher.utter_message(response="utter_list_todo", todo_list=todo_list)
+        return []
+    
+class CompleteTodo(Action):
+    def name(self) -> Text:
+        return "action_complete_todo"
+
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        todo_number = tracker.get_slot("todo_number")
+        todo_list = tracker.get_slot("todo_list")
+        del todo_list[todo_number-1]
+        print(todo_list)
+        todo_list = "".join([f"\n {idx+1}. {todo}" for idx,todo in enumerate(todo_list)])
+        dispatcher.utter_message(response="utter_list_todo", todo_list=todo_list)
+        return [SlotSet("todo_number", None), SlotSet("todo_list", todo_list)]
+    
+class CreateEvent(Action):
+    def name(self) -> Text:
+        return "action_create_event"
+
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        event = tracker.get_slot("event")
+        date_time = tracker.get_slot("event_datetime")
+        event = {}
+        event["summary"] = event
+        event["start"] = {"dateTime": date_time}
+        calendar_api.create_event(event)
+        dispatcher.utter_message(response="utter_event_added", event=event, date_time=date_time)
+        return [SlotSet("event", None), SlotSet("event_datetime", None)]
+    
+class ListEvent(Action):
+    def name(self) -> Text:
+        return "action_list_event"
+
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        events = calendar_api.list_recent_events()
+        dispatcher.utter_message(response="utter_list_event")
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            dispatcher.utter_message(response="utter_list_event", event=event['summary'], date_time=start)
         return []
